8a7eb8d61edaed53bac496cf259b7b1072fd2adf
Andrés J. Díaz <ajdiaz@ajdiaz.me>
2021-03-21 17:44:48 +0100

    bump: version 5.1.4
    Signed-off-by: Andrés J. Díaz <ajdiaz@ajdiaz.me>

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,9 +5,9 @@
 #
 # For Linux, also builds musl for truly static linking.
 
-bash_version="5.0"
-bash_patch_level=18
-musl_version="1.2.1"
+bash_version="5.1"
+bash_patch_level=4
+musl_version="1.2.2"
 
 platform=$(uname -s)
 
